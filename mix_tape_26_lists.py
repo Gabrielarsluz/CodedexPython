385be40fd913d4f2252916c13f964fc540eef828
Gabriela Luz <gabrielarsluz@gmail.com>
2025-07-09 21:17:36 -0300

    Adding more codedex python exercises, 25 and 26

---
+++ mix_tape_26_lists.py
@@ -0,0 +1,13 @@
+# 💿 Theme: Indie Travel Songs
+
+playlist = [
+  '1-Porches - rangerover',
+  '2-Mount Eerie - You Swan, Go On',
+  '3-Carolyn Polachek - Look at Me Now',
+  '4-Pinegrove - Darkness',
+  '5-LVL UP - Spirit Was',
+  '6-Mitski - First Love / Late Spring'
+]
+
+for song in playlist:
+    print(song)
